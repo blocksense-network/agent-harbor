1cf131df1b8a4040adc815c195c99dd84d5f2cb5
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-11-14 16:03:16 +0200

    feat(site): Create `mdx-components.js`

---
+++ docs/site/mdx-components.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2025 Schelling Point Labs Inc
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'; // nextra-theme-blog or your custom theme
+
+// Get the default MDX components
+const themeComponents = getThemeComponents();
+
+// Merge components
+export function useMDXComponents(components) {
+  return {
+    ...themeComponents,
+    ...components,
+  };
+}
