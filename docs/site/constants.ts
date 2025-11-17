134c8a00ecf5cbec5a5c7e3ca3e05db4c55c1894
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-11-17 12:28:13 +0200

    wip

---
+++ docs/site/constants.ts
@@ -0,0 +1,7 @@
+/**
+ * Copyright 2025 Schelling Point Labs Inc
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export const projectLink = 'https://github.com/blocksense-network/agent-harbor';
+export const docsRepositoryBase = 'https://github.com/blocksense-network/agent-harbor/blob/main/docs/site';
