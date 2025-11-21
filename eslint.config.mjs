2a902533a600777b7fafa966074d2c597b50a539
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-11-21 13:25:35 +0200

    config(eslint): Add shared config for the repo

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import solid from 'eslint-plugin-solid/configs/typescript';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
+import { flatConfigs as importConfigs } from 'eslint-plugin-import-x';
+
+export default defineConfig(
+  globalIgnores([
+    '**/dist',
+    '**/build',
+    '**/*.md',
+    '**/.vinxi',
+    '**/.output',
+    '**/.next',
+    '**/next-env.d.ts',
+    '.yarn/**',
+    '.pnp.cjs',
+    '.pnp.loader.mjs',
+    '.prettierrc.cjs',
+    'vendor',
+    '.obsidian',
+    'electron-app',
+  ]),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+  js.configs.recommended,
+  tseslint.configs.strict,
+  importConfigs.recommended,
+  importConfigs.typescript,
+  {
+    ...solid,
+    settings: {
+      'import-x/resolver-next': [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+          project: ['docs/*/tsconfig*.json', 'webui/**/tsconfig*.json'],
+        }),
+      ],
+    },
+
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'import-x/no-named-as-default-member': 'off',
+    },
+  },
+);
