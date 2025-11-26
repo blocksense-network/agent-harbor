b386b3b20a545e950fa1b54800bab4ed999a8624
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-11-26 11:40:22 +0200

    wip

---
+++ webui/app/.storybook/main.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2025 Schelling Point Labs Inc
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { defineMain } from 'storybook-solidjs-vite';
+
+export default defineMain({
+  framework: {
+    name: 'storybook-solidjs-vite',
+    options: {
+      // docgen: {
+      // Enabled by default, but you can configure or disable it:
+      //  see https://github.com/styleguidist/react-docgen-typescript#options
+      // },
+    },
+  },
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-vitest',
+  ],
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+});
