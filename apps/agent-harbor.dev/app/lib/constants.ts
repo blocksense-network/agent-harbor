0dd898a05e1bc3f976e8b85567effb042053bf1e
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-12-02 16:20:38 +0200

    feat(website): Create `contstants.ts` with urls

---
+++ apps/agent-harbor.dev/app/lib/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright 2025 Schelling Point Labs Inc
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export const siteName = 'Agent Harbor';
+export const siteUrl = 'https://agent-harbor.dev';
+export const opengraphImageUrl = `${siteUrl}/opengraph-image.png`;
+export const xUrl = 'https://x.com/agent_harbor';
